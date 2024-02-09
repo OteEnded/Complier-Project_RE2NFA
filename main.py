247fbb70fb54800a49765686f7db71082b4806f3
OteEnded <ratnaritjumnong@gmail.com>
2024-02-09 22:16:41 +0700

    start working

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-class NFA:
+# class NFA:
     
-    def __init__(self, states: list, alphabet: list, transitions: dict, start_state: int, accept_states: list):
-        self.states = states
-        self.alphabet = alphabet
-        self.transitions = transitions
-        self.start_state = start_state
-        self.accept_states = accept_states
+#     def __init__(self, states: list, alphabet: list, transitions: dict, start_state: int, accept_states: list):
+#         self.states = states
+#         self.alphabet = alphabet
+#         self.transitions = transitions
+#         self.start_state = start_state
+#         self.accept_states = accept_states
     
     
 def union(operand1, operand2):
     
     
     
-    return NFA()
+    print("nfa")
 
 
 def main():
